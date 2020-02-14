0608f9a26c35ff58f714fe07967ac1e7b4838403
PealMazumder <peal6614@gmail.com>
2020-02-15 00:28:18 +0600

    initial commit

--- Number Theory/Problems/Prime Number/Micro and Prime Prime.cpp
+++ Number Theory/Problems/Prime Number/Micro and Prime Prime.cpp
@@ -4,16 +4,16 @@ Problem Link : https://urlzs.com/iVtt5
 #include<bits/stdc++.h>
 #define	fast						ios::sync_with_stdio(false);cin.tie(0);cout.tie(NULL);
 #define all(p)						p.begin(),p.end()
-#define	mem(ar,val)						memset(ar, val, sizeof(ar))
+#define	mem(ar,val)					memset(ar, val, sizeof(ar))
 #define sz(c)						(int)c.size()
 #define pb(x)						push_back(x)
-#define mkp(x,y)						make_pair(x,y)
+#define mkp(x,y)					make_pair(x,y)
 #define ff						first
 #define ss						second
 #define ll						long long
 #define nl						"\n"
-#define MAX(a, b)						((a) > (b) ? (a) : (b))
-#define MIN(a, b)						((a) < (b) ? (a) : (b))
+#define MAX(a, b)					((a) > (b) ? (a) : (b))
+#define MIN(a, b)					((a) < (b) ? (a) : (b))
 #define FI						freopen("input.txt", "r", stdin);
 #define FO						freopen("output.txt", "w", stdout);
 using namespace std;
