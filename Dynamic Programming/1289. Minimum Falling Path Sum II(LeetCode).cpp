8a693755f2bd366d1122336539b65cc4f29f97ba
PealMazumder <peal6614@gmail.com>
2020-08-03 01:33:38 +0600

    initial commit

---
+++ Dynamic Programming/1289. Minimum Falling Path Sum II(LeetCode).cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& A) {
+        int n = (int)A.size();
+        int dp[n+2][n+2];
+        for(int i = 0; i<=n; i++)
+            dp[i][0] = dp[i][n+1] = INT_MAX;
+        for(int i = 0; i<n; i++)
+            dp[n][i+1] = A[n-1][i];
+        for(int i = n-1; i>=1; i--)
+        {
+           for(int j = 1; j<=n; j++)
+           {
+                int mn = INT_MAX;
+                for(int k = 1; k<=n; k++)
+                    if(k != j)
+                        mn = min(mn, dp[i+1][k]);
+                dp[i][j] = A[i-1][j-1] + mn;
+           }
+        }
+        int ans = INT_MAX;
+        for(int i = 1; i<=n; i++)
+            ans = min(ans, dp[1][i]);
+        return ans;
+    }
+};
