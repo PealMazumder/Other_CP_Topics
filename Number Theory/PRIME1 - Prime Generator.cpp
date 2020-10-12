cbf4d0b9022d698af445a1c20c0972d6f9a63f00
PealMazumder <peal6614@gmail.com>
2020-10-12 21:33:44 +0600

    Segmented Sieve of Eratosthenes

---
+++ Number Theory/PRIME1 - Prime Generator.cpp
@@ -0,0 +1,69 @@
+///Problem Link: https://www.spoj.com/problems/PRIME1/
+#include<bits/stdc++.h>
+#define	ll	long long
+#define	pb	push_back
+#define	sz(v)	(int)v.size()
+#define	nl	"\n"
+using namespace std;
+const int mx = 1000005;
+vector<int> prime;
+bool vis[mx];
+int casino = 0;
+void sieve()
+{
+	int x = sqrt(mx);
+	for(int i = 3; i <= x; i += 2)
+	{
+		if(vis[i] == 0)
+		{
+			for(int j = i*i; j<mx; j += 2*i)
+				vis[j] = 1;
+		}
+	}
+	prime.pb(2);
+	for(int i = 3; i<mx; i += 2)
+		if(vis[i] == 0)
+			prime.pb(i);
+}
+void segmentedSieve(ll a, ll b)
+{
+	if(b < 2) return;
+	if(a < 2) a = 2;
+	ll x = sqrt(b) + 1;
+	memset(vis,0,sizeof(vis));
+	for(ll i = 0; i < prime.size() && prime[i] <= x; i++)
+	{
+		ll j = prime[i] * prime[i];
+		if(j < a) // If j is smaller than a, then shift it inside of segment [a,b]
+			j = ((a + prime[i] - 1)/prime[i]) * prime[i];
+			
+		while(j <= b)
+		{
+			vis[j-a] = 1;
+			j += prime[i];	
+		}	
+		
+	}
+	for(ll i = a; i <= b; i++)
+		if(vis[i-a] == 0)
+			printf("%lld\n",i);
+	printf("\n");
+}
+void solve()
+{
+	ll a, b;
+	scanf("%lld %lld", &a, &b);
+	segmentedSieve(a,b);
+}
+int main()
+{
+	sieve();
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	    solve();
+
+	return 0;
+}
+
+
