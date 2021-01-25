dac9ec1c7e21ce99b979267d5e8a1d987f8bedd7
PealMazumder <peal6614@gmail.com>
2021-01-25 10:42:40 +0600

    Sieve

---
+++ Number Theory/Prime Tuples.cpp
@@ -0,0 +1,79 @@
+//Problem Link: https://www.codechef.com/COOK126B/problems/PTUPLES/
+#include<bits/stdc++.h>
+#define		nl			"\n"
+#define		mod			1000000007
+#define		sz(c)			(int)c.size()
+#define		pb			push_back
+#define		mkp			make_pair
+#define 	deb			cout<<"ok\n"
+#define		ll			long long
+#define		pii			pair<int, int>
+#define 	pll			pair<ll, ll>
+#define 	vll			vector<ll>
+#define 	vi			vector<int>
+#define		no			cout<<"NO\n"
+#define		yes			cout<<"YES\n"
+#define		gcd(a, b)		__gcd(a, b)
+#define		total_bit(a)		__builtin_popcount(a)
+#define		lcm(a, b)		((a)*((b)/gcd(a,b)))
+#define		REP(i,n)		for(int i = 0; i<n; i++)
+#define		all(p)			p.begin(),p.end()
+#define		mem(ar,val)		memset(ar, val, sizeof(ar))
+#define 	UNIQUE(a)		sort(all(a)); a.erase(unique(all(a)), a.end())
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const double pi = acos(-1.0);
+const ll INF = 1000000000000000000;
+const ll N = 1e6 + 1;
+vector<ll> prime;
+bool vis[N];
+int arr[N];
+void sieve()
+{
+	for(ll i = 3; i*i<=N; i+=2)
+	{
+		if(!vis[i])
+		{
+			for(ll j = i*i; j<=N; j += 2*i)
+				vis[j] = true;
+		}
+	}
+	prime.push_back(2LL);
+	for(ll i = 3; i<N; i += 2)
+		if(!vis[i]) prime.push_back(i);
+	
+	for(int i = 0; i<sz(prime)-1; i++)
+	{
+		if(prime[i+1] - prime[i] == 2)
+			arr[prime[i+1]] = 1;
+	}
+	int cnt = 1;
+	for(int i = 6; i<=N; i++)
+	{
+		if(arr[i] == 1)
+			cnt++, arr[i] = cnt;
+		else arr[i] = cnt;
+	}
+}
+void solve()
+{
+	ll n, i, j;
+	cin>>n;
+	cout<<arr[n]<<nl;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);cout.tie(NULL);
+	sieve();
+	ll t;
+	cin>>t;
+	while(t--)
+		solve();
+	return 0;
+}
+
+
