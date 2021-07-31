d0c36ffa6b03ce2f97cf6b0f01ceccb112e31989
Peal Mazumder <peal6614@gmail.com>
2021-07-31 19:57:04 +0600

    priority queue(min heap)

---
+++ STL/D - Querying Multiset.cpp
@@ -0,0 +1,74 @@
+//Problem Link: https://atcoder.jp/contests/abc212/tasks/abc212_d
+#include<bits/stdc++.h>
+#define         nl                  "\n"
+#define         mod                 1000000007
+#define         sz(c)               (int)c.size()
+#define         pb                  push_back
+#define         ff                  first
+#define         ss                  second
+#define         mkp                 make_pair
+#define         ll                  long long
+#define         pii                 pair<int, int>
+#define         pll                 pair<ll, ll>
+#define         vll                 vector<ll>
+#define         vi                  vector<int>
+#define         no                  cout<<"NO\n"
+#define         yes                 cout<<"YES\n"
+#define         one                 cout<<"1\n"
+#define         mone                cout<<"-1\n"
+#define         gcd(a, b)           __gcd(a, b)
+#define         total_bit(a)        __builtin_popcount(a)
+#define         lcm(a, b)           ((a)*((b)/gcd(a,b)))
+#define         all(p)              p.begin(),p.end()
+#define         mem(ar,val)         memset(ar, val, sizeof(ar))
+#define         UNIQUE(a)           sort(all(a)); a.erase(unique(all(a)), a.end())
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const double pi = acos(-1.0);
+const ll INF = 1000000000000000000;
+const int N = 1e6+7;
+
+void solve()
+{
+    ll q;
+    cin>>q;
+    priority_queue <ll, vector<ll>, greater<ll> > pq;
+    ll sm = 0, type, x;
+    while(q--)
+    {
+        cin>>type;
+        if(type == 1)
+        {
+            cin>>x;
+            pq.push(x - sm);
+        }
+        else if( type == 2)
+        {
+            cin>>x;
+            sm += x;
+        }
+        else
+        {
+            ll tm = pq.top();
+            pq.pop();
+            cout<<tm + sm<<nl;
+        }
+    }
+
+}
+int main()
+{
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(NULL);
+//    int t;
+//    cin>>t;
+//    while(t--)
+        solve();
+    return 0;
+}
+
